5d14eddfea0a8cb1f24a5763f6b5876bb6ed322a
Brian Coords <bacoords@gmail.com>
2016-12-13 08:06:24 -0800

    adds redirect for logged out users

--- functions.php
+++ functions.php
@@ -152,3 +152,31 @@ add_action('do_feed_rss2', 'wpb_disable_feed', 1);
 add_action('do_feed_atom', 'wpb_disable_feed', 1);
 add_action('do_feed_rss2_comments', 'wpb_disable_feed', 1);
 add_action('do_feed_atom_comments', 'wpb_disable_feed', 1);
+
+
+
+
+
+add_action( 'parse_request', 'journal_wp_theme_redirect_to_login_if_not_logged_in', 1 );
+/**
+ * Redirects a user to the login page if not logged in.
+ *
+ * @author Daan Kortenbach
+ */
+function journal_wp_theme_redirect_to_login_if_not_logged_in() {
+	is_user_logged_in() || auth_redirect();
+}
+
+
+add_filter( 'login_url', 'journal_wp_theme_strip_loggedout', 1, 1 );
+/**
+ * Strips '?loggedout=true' from redirect url after login.
+ *
+ * @author Daan Kortenbach
+ *
+ * @param  string $login_url
+ * @return string $login_url
+ */
+function journal_wp_theme_strip_loggedout( $login_url ) {
+	return str_replace( '%3Floggedout%3Dtrue', '', $login_url );
+}
