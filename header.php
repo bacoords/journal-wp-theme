25b820192a0c09709ed442e4623d8f3ed3bb9ded
Brian Coords <bacoords@gmail.com>
2016-12-13 07:57:41 -0800

    add nav menu

--- header.php
+++ header.php
@@ -36,12 +36,12 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-<!--
+
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'journal-wp-theme' ); ?></button>
+<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'journal-wp-theme' ); ?></button>-->
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav>
--->
+
 		
 	</header><!-- #masthead -->
 
