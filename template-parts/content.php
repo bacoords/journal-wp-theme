71adfd4fd5ee546ec4e5820472e69370115367d8
Brian Coords <bacoords@gmail.com>
2017-03-22 07:04:19 -0700

    full text in feed

--- template-parts/content.php
+++ template-parts/content.php
@@ -32,15 +32,15 @@
 
 	<div class="entry-content">
 		<?php
-      if ( is_single() ) :
+//      if ( is_single() ) :
         the_content( sprintf(
           /* translators: %s: Name of current post. */
           wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'journal-wp-theme' ), array( 'span' => array( 'class' => array() ) ) ),
           the_title( '<span class="screen-reader-text">"', '"</span>', false )
         ) );
-      else :
-        the_excerpt();
-      endif;
+//      else :
+//        the_excerpt();
+//      endif;
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'journal-wp-theme' ),
